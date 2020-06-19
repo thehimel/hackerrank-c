2cd242f56d52ef4cf90d75baa7ddc27220869aea
Himel Das <himelhub@gmail.com>
2020-06-19 21:38:39 +0600

    refactor

---
+++ i03_sum_of_digits.c
@@ -0,0 +1,34 @@
+/*
+https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/problem?h_r=next-challenge&h_v=zen
+
+Task: Given a five digit number, return the sum of the digits.
+
+Input
+10564
+
+Output
+16
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() {
+    int num, last_digit;
+    scanf("%d", &num);
+
+    int sum = 0;
+
+    while(num > 0){
+        last_digit = num % 10;
+        sum += last_digit;
+        num /= 10;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
